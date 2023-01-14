0539dc499a2d37c4191333fb5b8e044276edf3c4
Jaydeep-07 <jaydeepvpatil225@gmail.com>
2023-01-14 10:39:02 +0530

    Add project files.

---
+++ MemoryCacheDemo/Data/DbContextClass.cs
@@ -0,0 +1,14 @@
+﻿using MemoryCacheDemo.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace MemoryCacheDemo.Data
+{
+    public class DbContextClass: DbContext
+    {
+        public DbContextClass(DbContextOptions<DbContextClass> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
